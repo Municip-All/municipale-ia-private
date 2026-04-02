2a2d7f4fa42cad57283c83fc8dba7317464a553a
Kaiser <enzo.biger@gmail.com>
2026-04-02 11:53:57 +0200

    init qdrant

--- qdrant/main.py
+++ qdrant/main.py
@@ -1,3 +1,9 @@
 from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance, PointStruct
+import os
 
-client = QdrantClient(url="http://localhost:6333")
+
+client = QdrantClient(url="http://localhost:6333", api_key= os.environ.get('QDRANT_API_KEY'))
+
+collection = QdrantClient.create_collection(collection_name='reports')
+
